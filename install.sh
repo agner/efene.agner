b1b91580bcb1f8a5e93f7c044bd5a50da47f9d6b
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-15 12:33:57 -0800

    Workaround for efene's standard library availability

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 cp -rv * $AGNER_INSTALL_PREFIX
+cp $AGNER_INSTALL_PREFIX/lib/*.beam $AGNER_INSTALL_PREFIX/ebin
 echo "#! /bin/sh" > $AGNER_INSTALL_PREFIX/bin/efene
 echo "FNPATH=$AGNER_INSTALL_PREFIX $AGNER_INSTALL_PREFIX/bin/fnc \$*" >> $AGNER_INSTALL_PREFIX/bin/efene
 chmod +x $AGNER_INSTALL_PREFIX/bin/efene
